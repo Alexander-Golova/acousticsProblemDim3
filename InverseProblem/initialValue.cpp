407cdb1fc9c11de39387c585a6aa5c16d2080b33
alexander kozlov <al.iv.kozlov@yandex.ru>
2018-03-26 00:33:04 +0300

    Добавлена начальная инициализация

---
+++ InverseProblem/initialValue.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "initialValue.h"
+#include "../directProblem/Sources.h"
+
+using namespace std;
+
+void InitialValueU(const size_t numberSource, vector<vector<complex<float>>> & u, 	vector<complex<float>> & Source_R)
+{
+	for (size_t count = 0; count < numberSource; ++count)
+	{
+		for (size_t i = 0; i < N; ++i)
+		{
+			for (size_t j = 0; j < N; ++j)
+			{
+				for (size_t k = 0; k < N; ++k)
+				{
+					u[count][i * N_SQUARED + j * N + k] = Source_R[count * N_QUBE + i * N_SQUARED + j * N + k];
+				}
+			}
+		}
+	}
+}
+
+void InitialValueXi(vector<complex<float>> & xi)
+{
+	for (size_t i = 0; i < N_QUBE; ++i)
+	{
+		xi[i] = static_cast<complex<float>>(0.0f);
+	}
+}
---
+++ InverseProblem/initialValue.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "../directProblem/BasicDataProblem.h"
+#include "../directProblem/Sources.h"
+#include <vector>
+#include <complex>
+
+void InitialValueU(const size_t numberSource, std::vector<std::vector<std::complex<float>>> & u, std::vector<std::complex<float>> & Source_R);
+
+void InitialValueXi(std::vector<std::complex<float>> & xi);
+
